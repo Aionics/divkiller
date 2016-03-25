a03a8e55cd9905038b3100a2df45d4efbb4c7ca9
Kosarev Artem <Aionics@list.ru>
2016-03-25 22:53:55 +0500

    s

--- sas2.js
+++ sas2.js
@@ -2,15 +2,13 @@ function Vec(x, y){  //class vector
     this.x = x;
     this.y = y;
     var self = this;
-    this.addition= function (vec){    
-        self.x += vec.x;
-        self.y += vec.y;
-        return self;
+    this.addition= function (vec){
+        vec2 = new Vec(self.x + vec.x, self.y + vec.y)    
+        return vec2;
     };
     this.subtract = function (vec){
-        self.x -= vec.x;
-        self.y -= vec.y;
-        return self;
+        vec2 = new Vec(self.x - vec.x, self.y - vec.y)
+        return vec2;
     };
     this.multiply = function(number){
         self.x *= number;
@@ -19,17 +17,10 @@ function Vec(x, y){  //class vector
     };
 }
 
-Vec.prototype
-
-
 function entity(size, spawnPos, color){ // entity cretae constructor
     this.pos = spawnPos;
     this.size = size;
     this.color = color;
-
-    this.setPos = function(pos) {
-        this.pos = pos;
-    };
     this.draw = function() {
 
         context.fillStyle = this.color;
@@ -42,8 +33,7 @@ function entity(size, spawnPos, color){ // entity cretae constructor
         context.fill();
     };
 }
-function background(x, y, width, height) 
-{
+function background(x, y, width, height){
     this.x = x;
     this.y = y;
     this.width = width; 
@@ -53,29 +43,43 @@ function background(x, y, width, height)
         context.globalAlpha = globalAlpha;
         context.fillStyle = color;
         context.fillRect(this.x, this.y, this.width, this.height);
+        context.globalAlpha = 1
     };
+}
+function rope(){
+    this.vecStart = new Vec(0, 0);
+    this.vecEnd = new Vec(0, 0);
+    var self = this;
+    this.draw = function(vecS, vecE){
+        self.vecStart = vecS;
+        self.vecEnd = vecE;
+        context.beginPath();
+        context.moveTo(self.vecStart.x, self.vecStart.y);
+        context.lineTo(self.vecEnd.x, self.vecEnd.y);
+        context.stroke();
+    }
 }
 
 function handleMouseMove(event) {
-    cursor = new Vec(event.clientX, event.clientY);
+    cursor.x = event.clientX;
+    cursor.y = event.clientY;
 }
 
 function update() {
-    pointer.setPos(cursor);
-    follower.oldPos = follower.pos;
-    var ad = (subtract = function subtract(vec1, vec2){
-    // console.log(vec1.x + '   -   '+ vec2.x)
-    var vec3 = new Vec(
-        vec1.x - vec2.x,
-        vec1.y - vec2.y
-    );
-    return vec3
-}(cursor, follower.pos)).multiply(0.03);
-    console.log(ad);
-    follower.pos = addition(follower.pos, ad)
+    // pointer.pos = pointer.pos.addition( cursor.subtract(pointer.pos).multiply(0.1)); // pointer moving
+    // HOWITLOOKS: pointer.pos = pointer.pos + ( cursor.pos - pointer.pos ) * 0.3
+    pointer.pos = cursor; // pointer moving
+
+
+    follower.oldPos = follower.pos; //follower moving
+    follower.pos = follower.pos.addition( cursor.subtract(follower.pos).multiply(0.03).addition( follower.direction.multiply(1.94) ).multiply(0.5) )
+    // HOWITLOOKS: follower.pos = follower.pos + ( (cursor.pos - follower.pos) * 0.03 + follower.velocity ) / 2  ; mass = 1.90
+    follower.direction = follower.pos.subtract(follower.oldPos);
 }
-function draw() {
-    game.draw();
+function draw() {    
+    gameFiled.draw('grey', 1);
+    rope.draw(pointer.pos, follower.pos);
+    follower.draw();
     pointer.draw();
 }
 function main()
@@ -90,11 +94,20 @@ function main()
 
 var it = 0;
 
-var game = new background(0, 0, 480, 320); // прямоугольник закрашивающий фон
-var cursor;
+var gameFiled = new background(0, 0, window.innerWidth - 10, window.innerHeight - 10);
+var cursor = new Vec(1, 1);
 
-var pointer = new entity(15, new Vec(50, 50), 'blue'); // шар
-var follower = new entity(20, new Vec(50, 50), 'red');
+
+var pointer = new entity(15, new Vec(50, 50), 'blue'); //mainEntity
+document.body.style.cursor = 'none';
+var rope = new rope();
+
+var follower = new entity(20, new Vec(50, 50), 'red'); //followerEntity
+follower.direction = new Vec(0, 0);
+
+enemies = {
+    'default': [],
+}
 
 document.onmousemove = handleMouseMove;
 
@@ -102,7 +115,7 @@ var canvas = document.getElementById("myCanvas");
 canvas.style.position = 'absolute';
 canvas.style.top = '0';
 canvas.style.left = '0';
-canvas.width = game.width; // ширина холста
-canvas.height = game.height; // высота холста
+canvas.width = gameFiled.width;
+canvas.height = gameFiled.height;
 context = canvas.getContext("2d");
-setInterval(main, 1000/ 60); //отрисовываем 50 раз за секунду
+setInterval(main, 1000/ 60);
